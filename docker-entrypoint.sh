4676bf87f7a3ad8f9dfa205d74d862b0e0ab1f4e
Meng Sen <qyg2297248353@gmail.com>
2024-08-21 16:57:14 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,14 +3,13 @@
 TARGET_DIR="/app"
 TEMP_DIR="/tmp/app-temp"
 
-if [ -f "$TARGET_DIR/.env" ]; then
-    echo ".env file exists, not overwriting."
-else
+if [ ! -f "$TARGET_DIR/.env" ]; then
     cp $TEMP_DIR/.env $TARGET_DIR/
 fi
 
-rsync -a --exclude '.env' $TEMP_DIR/ $TARGET_DIR/
+rsync -av --exclude '.env' $TEMP_DIR/ $TARGET_DIR/
 
 chown -R www-data:www-data $TARGET_DIR
 
+php artisan queue:work >/tmp/work.log 2>&1 &
 exec supervisord
